f4471b7e3c22faf5f65cd3c80828d30ac56a66c6
Yuliya Karuk <karuk.yuliya@gmail.com>
2024-07-05 20:29:16 +0300

    feat: add logout functionality

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,3 +1,5 @@
+import { useToast } from '@contexts/toastProvider';
+import { auth } from '@firebase/firebase';
 import { useClickOutside } from '@hooks/useClickOutside';
 import { AppRoutes } from '@router/routes';
 import classnames from 'classnames';
@@ -7,11 +9,24 @@ import { Link } from 'react-router-dom';
 import s from './Navigation.module.scss';
 
 const paths: string[] = [AppRoutes.LOGIN_ROUTE, AppRoutes.REGISTRATION_ROUTE];
+// const logginedPaths: string[] = [AppRoutes.LOGIN_ROUTE, AppRoutes.REGISTRATION_ROUTE];
 
 export const Navigation = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const isDesktop = useMediaQuery({ query: '(min-width: 769px)' });
   const menuRef = useRef(null);
+  const { successNotify, errorNotify } = useToast();
+
+  const logout = () => {
+    auth
+      .signOut()
+      .then(() => {
+        successNotify('User signed out successfully');
+      })
+      .catch(error => {
+        errorNotify((error as Error).message);
+      });
+  };
 
   const handleMenuToggle = () => {
     setIsMenuOpen(prev => !prev);
@@ -51,12 +66,15 @@ export const Navigation = () => {
       <nav className={classnames(s.menu, { [s.active]: isMenuOpen })} ref={menuRef}>
         <ul className={s.menuList}>
           {paths.map(path => (
-            <li key={path} className={s.menuItem}>
-              <Link to={path} onClick={handleMenuToggle}>
+            <li key={path}>
+              <Link to={path} onClick={handleMenuToggle} className={s.menuItem}>
                 {path.slice(1)[0].toUpperCase() + path.slice(2)}
               </Link>
             </li>
           ))}
+          <button type="button" className={s.menuItem} onClick={logout}>
+            Logout
+          </button>
         </ul>
       </nav>
     </>
