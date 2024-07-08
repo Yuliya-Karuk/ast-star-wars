bc0929f7cebade6b06b657efef41b72fe0978e2b
Yuliya Karuk <karuk.yuliya@gmail.com>
2024-07-08 20:23:35 +0300

    fix: remove component MainLayout

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,18 +1,18 @@
+// import { MainLayout } from '@components/index';
 import { Home, Login, NotFound, Registration } from '@pages/index';
 import { SearchPage } from '@pages/searchPage/searchPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { MainLayout } from '../components';
 import { AppRoutes } from './routes';
 
 export const AppRouter = () => (
   <BrowserRouter>
     <Routes>
-      <Route element={<MainLayout />}>
-        <Route path={AppRoutes.HOME_ROUTE} element={<Home />} />
-        <Route path={AppRoutes.REGISTRATION_ROUTE} element={<Registration />} />
-        <Route path={AppRoutes.LOGIN_ROUTE} element={<Login />} />
-        <Route path={AppRoutes.SEARCH_ROUTE} element={<SearchPage />} />
-      </Route>
+      {/* <Route element={<MainLayout />}> */}
+      <Route path={AppRoutes.HOME_ROUTE} element={<Home />} />
+      <Route path={AppRoutes.REGISTRATION_ROUTE} element={<Registration />} />
+      <Route path={AppRoutes.LOGIN_ROUTE} element={<Login />} />
+      <Route path={AppRoutes.SEARCH_ROUTE} element={<SearchPage />} />
+      {/* </Route> */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
