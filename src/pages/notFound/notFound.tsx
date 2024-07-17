a01d1abb9a9c3037b38baecc1ccdf308d4f8f313
Yuliya Karuk <karuk.yuliya@gmail.com>
2024-07-17 17:35:46 +0300

    feat: add notFound Page

--- src/pages/notFound/notFound.tsx
+++ src/pages/notFound/notFound.tsx
@@ -1,3 +1,17 @@
-export const NotFound = () => {
-  return <h1>404</h1>;
-};
+import notFoundImage from '@assets/404.png';
+import { AppRoutes } from '@router/routes';
+import { Link } from 'react-router-dom';
+import styles from './notFound.module.scss';
+
+export function NotFound() {
+  return (
+    <div className={styles.notFound}>
+      <div className={styles.container}>
+        <img className={styles.notFoundImage} src={notFoundImage} alt="Icon Not Found" />
+        <Link className={styles.notFoundLink} to={AppRoutes.HOME_ROUTE}>
+          Back to Home page
+        </Link>
+      </div>
+    </div>
+  );
+}
